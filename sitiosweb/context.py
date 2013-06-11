b7b181762eb6423e66289fa82d5b72bd58ac6168
charlieweb <charlyesp82@gmail.com>
2013-06-11 17:17:47 -0600

    ads

--- sitiosweb/context.py
+++ sitiosweb/context.py
@@ -3,6 +3,6 @@ from noticias.models import Noticias
 
 def globales(request):
 	evento = Eventos.objects.order_by('-id')[:3]
-	noticia = Noticias.objects.order_by('-id')[:3]
+	noticia = Noticias.objects.order_by('-id')[:4]
 
 	return {'eventos':evento, 'noticias':noticia}
