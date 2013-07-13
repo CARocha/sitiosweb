89ac4f494757215f59e672b7bb0e59ec0de47795
sacrac <crocha09.09@gmail.com>
2013-07-12 20:50:29 -0600

    fix GSE

--- googlesearch/templatetags/googlesearch_inclusion_tags.py
+++ googlesearch/templatetags/googlesearch_inclusion_tags.py
@@ -6,9 +6,9 @@ register = template.Library()
 
 @register.inclusion_tag('googlesearch/inclusion_tags/form.html', takes_context=True)
 def googlesearch_form(context):
-    context.update({
-        'google_search_partner_id': settings.GOOGLE_SEARCH_PARTNER_ID
-    })
+    #context.update({
+    #    'google_search_partner_id': settings.GOOGLE_SEARCH_PARTNER_ID
+    #})
     return context
 
 
